68cc954e9f76b11d8691ee63d2fafd1c7eaee6c3
mehdidhammou <mehdidhammou@protonmail.com>
2022-01-13 16:45:10 +0100

    inventory changes [index]

--- src/source/classes/Inventory.java
+++ src/source/classes/Inventory.java
@@ -1,4 +1,5 @@
 package source.classes;
+import java.util.LinkedList;
 import java.util.Vector;
 import source.services.FileHandler;
 
@@ -12,7 +13,7 @@ public class Inventory {
         }else return ChangeProdQuantity(prod.getRef(), qte);
     }
 
-        public static boolean ChangeProdQuantity(String ref, int qte){
+    public static boolean ChangeProdQuantity(String ref, int qte){
         String temp = DoesExist(ref);
         if(temp == null ) return false;
         String temp1 = temp.replace(temp.split("|")[6], String.valueOf(qte));
@@ -20,11 +21,11 @@ public class Inventory {
         return true;
     }
 
-        public static String DoesExist(String ref){
+    public static String DoesExist(String ref){
         return FileHandler.GetDataByRef(InventoryPath, ref);
     }
 
-        public static boolean ChangePrice(String ref, float price){
+    public static boolean ChangePrice(String ref, float price){
         String temp = DoesExist(ref);
         if(temp == null) return false;
         String temp1 = temp.replace(temp.split("|")[5], String.valueOf(price));
@@ -32,12 +33,12 @@ public class Inventory {
         return true;
     }
 
-        static int AvailableQuantity(String ref){
+    public static int AvailableQuantity(String ref){
             String temp = DoesExist(ref);
             return Integer.parseInt(temp.split("|")[6]);
         }
 
-        public static Product[] GetProducts(){
+    public static Product[] GetProducts(){
         String temp = FileHandler.GetContent(InventoryPath);
         String[] tempArray = temp.split("\n");
         int size = tempArray.length;
@@ -48,23 +49,23 @@ public class Inventory {
         return result;
     }
 
-        public static String GetProdNameByRef(String ref){
+    public static String GetProdNameByRef(String ref){
         if(DoesExist(ref) != null) return FileHandler.GetDataByRef(InventoryPath, ref).split("|")[2];
         return null;
     }
 
-        public static Product[] SearchProducts(String method, String key) {
-            Product[] temp = GetProducts();
-            Vector<Product> result = new Vector<Product>(); 
-            for (Product product : temp) {
-                String field = method == "name" ?product.getName()
-                            : method == "ref" ?product.getRef()
-                            : method == "desc" ?product.getDescription()
-                            : method == "spec" ?product.getSpecs()
-                            : null;
-                if (field == null) return null;
-                if (field.indexOf(key) != -1) result.addElement(product);
+    public static Product[] SearchProducts(String method, String key) {
+        Product[] temp = GetProducts();
+        LinkedList<Product> result = new LinkedList<Product>(); 
+        for (Product product : temp) {
+            String field = method == "name" ?product.getName()
+                    : method == "ref" ?product.getRef()
+                    : method == "desc" ?product.getDescription()
+                    : method == "spec" ?product.getSpecs()
+                    : null;
+            if (field == null) return null;
+            if (field.indexOf(key) != -1) result.add(product);
             }
-            return (Product[])result.toArray();
-        }    
+        return (Product[])result.toArray();
+    }
 }
